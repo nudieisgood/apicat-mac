d1c6d27df7e2a14810e78f383757e89996554156
jeremychan523 <jeremychan523@wonder4studio.com>
2024-02-02 22:46:36 +0800

    fix:修正linux系統打包出錯問題

--- forge.config.js
+++ forge.config.js
@@ -3,54 +3,74 @@ const today = dayjs().format("YYYYMMDD");
 
 module.exports = {
   packagerConfig: {
-    appVersion:`${today}`,
-    icon:"./public/favicon.ico",
-    name:"ApiCat",
+    //安裝後應用入口文件的圖標,不用後綴會自動根據打包的作業系統加上對應的圖片後綴
+    icon: "./public/favicon",
+    //安裝後應用入口的檔名
+    name: "ApiCat",
+    executableName: "ApiCat",
     asar: true,
   },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {
+        name: `ApiCat_${today}`,
+        //安裝exe檔檔名
+        setupExe: `ApiCat_${today}.exe`,
         setupIcon: "./public/favicon.ico",
       },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-dmg",
+      config: {
+        //安裝dmg檔檔名
+        name: `ApiCat_${today}.dmg`,
+        icon: "./public/favicon.icns",
+      },
     },
     {
-      name: '@electron-forge/maker-deb',
-      config: { 
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {
         options: {
-          icon: './public/favicon.png',
-        }},
+          icon: "./public/favicon.png",
+          name: `ApiCat_${today}`,
+        },
+      },
     },
     {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      name: "@electron-forge/maker-rpm",
+      config: {
+        options: {
+          icon: "./public/favicon.png",
+          name: `ApiCat_${today}`,
+        },
+      },
     },
   ],
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
     {
-      name: '@electron-forge/plugin-webpack',
+      name: "@electron-forge/plugin-webpack",
       config: {
-        mainConfig: './webpack.main.config.js',
+        mainConfig: "./webpack.main.config.js",
         renderer: {
-          config: './webpack.renderer.config.js',
+          config: "./webpack.renderer.config.js",
           entryPoints: [
             {
               // js: "./src/modules/main/renderer.js", //測試用
               html: "./public/main.html",
               js: "./src/modules/main/main.js",
-              name: 'main_window',
+              name: "main_window",
               preload: {
-                js: './src/preload.js',
+                js: "./src/preload.js",
               },
             },
           ],
